b04439523109772b8088113ee5e6a2d3ff0b71ad
pablo-caparelli <pablocaparelli@gmail.com>
2026-04-20 15:35:25 -0300

    instalación, importación y conexión con mongodb

--- server.js
+++ server.js
@@ -1,101 +1,134 @@
 import express from "express";
+import mongoose from "mongoose";
+
+const connectMongoDb = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/db_tp3_node");
+    console.log("✅ Conectado a MongoDB con éxito");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+//esquema -> schema
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+const Product = mongoose.model("Product", productSchema);
 
 const server = express();
-
 server.use(express.json());
 
-let products = [
-  { id: crypto.randomUUID(), name: "Laptop", price: 1200 },
-  { id: crypto.randomUUID(), name: "Mouse", price: 25 },
-  { id: crypto.randomUUID(), name: "Teclado", price: 80 },
-];
+// let products = [
+//   { name: "Laptop", price: 1200, available: true },
+//   { name: "Mouse", price: 25 },
+//   { name: "Teclado", price: 80 },
+// ];
 
 server.get("/products", (req, res) => {
   res.json(products);
 });
 
-server.get("/products/:id", (req, res) => {
-  const { id } = req.params;
+// server.get("/products/:id", (req, res) => {
+//   const { id } = req.params;
 
-  const product = products.find((p) => p.id.trim() === id.trim());
+//   const product = products.find((p) => p.id.trim() === id.trim());
 
-  if (!product) {
-    return res.status(404).json({ message: "Producto no encontrado" });
-  }
+//   if (!product) {
+//     return res.status(404).json({ message: "Producto no encontrado" });
+//   }
 
-  res.json(product);
-});
+//   res.json(product);
+// });
 
-server.post("/products", (req, res) => {
-  const { name, price } = req.body;
+// server.post("/products", (req, res) => {
+//   const { name, price } = req.body;
 
-  if (!name || !price) {
-    return res
-      .status(400)
-      .json({ message: "La información de name y price debe estar completa" });
-  }
+//   if (!name || !price) {
+//     return res
+//       .status(400)
+//       .json({ message: "La información de name y price debe estar completa" });
+//   }
 
-  const newProduct = {
-    id: crypto.randomUUID(),
-    name,
-    price,
-  };
+//   const newProduct = {
+//     id: crypto.randomUUID(),
+//     name,
+//     price,
+//   };
 
-  products.push(newProduct);
+//   products.push(newProduct);
 
-  res.status(201).json({
-    message: "Producto agregado correctamente",
-    product: newProduct,
-  });
-});
+//   res.status(201).json({
+//     message: "Producto agregado correctamente",
+//     product: newProduct,
+//   });
+// });
 
-server.delete("/products/:id", (req, res) => {
-  const { id } = req.params;
+// server.delete("/products/:id", (req, res) => {
+//   const { id } = req.params;
 
-  const product = products.find((p) => p.id === id);
+//   const product = products.find((p) => p.id === id);
 
-  if (!product) {
-    return res.status(404).json({ message: "Producto no encontrado" });
-  }
+//   if (!product) {
+//     return res.status(404).json({ message: "Producto no encontrado" });
+//   }
 
-  products = products.filter((p) => p.id !== id);
+//   products = products.filter((p) => p.id !== id);
 
-  res.json({
-    message: "Producto eliminado correctamente",
-    product: product,
-  });
-});
+//   res.json({
+//     message: "Producto eliminado correctamente",
+//     product: product,
+//   });
+// });
 
-server.put("/products/:id", (req, res) => {
-  const { id } = req.params;
-  const { name, price } = req.body;
+// server.put("/products/:id", (req, res) => {
+//   const { id } = req.params;
+//   const { name, price } = req.body;
 
-  const index = products.findIndex((p) => p.id === id);
+//   const index = products.findIndex((p) => p.id === id);
 
-  if (index === -1) {
-    return res.status(404).json({ message: "Producto no encontrado" });
-  }
+//   if (index === -1) {
+//     return res.status(404).json({ message: "Producto no encontrado" });
+//   }
 
-  if (!name || !price) {
-    return res
-      .status(400)
-      .json({ message: "La información de name y price debe estar completa" });
-  }
+//   if (!name || !price) {
+//     return res
+//       .status(400)
+//       .json({ message: "La información de name y price debe estar completa" });
+//   }
 
-  const updatedProduct = {
-    id,
-    name,
-    price,
-  };
+//   const updatedProduct = {
+//     id,
+//     name,
+//     price,
+//   };
 
-  products[index] = updatedProduct;
+//   products[index] = updatedProduct;
 
-  res.json({
-    message: "Producto actualizado correctamente",
-    product: updatedProduct,
-  });
-});
+//   res.json({
+//     message: "Producto actualizado correctamente",
+//     product: updatedProduct,
+//   });
+// });
 
 server.listen(3000, () => {
+  connectMongoDb();
   console.log(`✅ Servidor en escucha http://localhost:3000`);
 });
